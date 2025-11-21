6720493c45f3d8626d2f3990f16b13356b2b39c1
antoniosvpaim-create <antoniosvpaim@outlook.com>
2025-11-21 19:10:57 -0300

    mais um arquivo

--- mvp.py
+++
@@ -1,7 +0,0 @@
-
-
-lista = []
-
-for i in range(1,11):
-    lista.append(i)
-    print(lista[i-1])
--- soma.py
+++
@@ -1,6 +0,0 @@
-a=20
-
-for i in range(10):
-    print("MY DICK IS little BIG")
-
-print("Atualização teste")
